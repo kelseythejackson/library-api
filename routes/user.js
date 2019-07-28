915b719e4aba0599baadca6bc2f9f2fe414f64ad
Kelsey Jackson <kelseythej@gmail.com>
2019-07-27 23:33:43 -0500

    finished video-47

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 import Router from 'koa-router';
 import bcrypt from 'bcrypt';
 import { promisify } from 'util';
+import currentUser from '../middleware/current-user';
 
 const hash = promisify(bcrypt.hash);
 
@@ -13,6 +14,11 @@ router.post('/', async (ctx) => {
   const user = await ctx.app.db.User.create(attrs);
 
   ctx.body = ctx.app.serialize('user', user);
-})
+});
+
+router.get('/me', currentUser, async (ctx) => {
+  
+  ctx.body = ctx.app.serialize('user', ctx.currentUser);
+});
 
 export default router.routes();
